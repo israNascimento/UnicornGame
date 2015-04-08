03be35c718f0090503a1d50b37ac390089f473e8
israNascimento <israelnascimento.nave@gmail.com>
2015-04-08 14:11:12 -0300

    First Commit

---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movimentation")]
+    public float speed;
+
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update () 
+	{
+		if (Input.acceleration.x != 0) 
+		{
+            rigidbody2D.AddForce(new Vector2(Input.acceleration.x*50, 0));
+		}
+	}
+}
