363611c270ea705b64fbe982645093f07d8356ec
israNascimento <israelnascimento.nave@gmail.com>
2015-05-20 10:43:15 -0300

    Animação do menu

---
+++ Assets/Scripts/Menu/Transition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transition : MonoBehaviour 
+{
+    public GameObject[] buttons;
+    Animator anim;
+	
+    public void ChangeScene(string scene)
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            print("JJ");
+            buttons[i].GetComponent<Animator>().SetBool("GoConfig", true);
+        }
+    }
+}
