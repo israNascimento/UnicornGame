a552e7e7ad5594f990be0cbe4167afc75d77d16a
israNascimento <israelnascimento.nave@gmail.com>
2015-05-22 15:01:01 -0300

    Animação do menu principal OK

--- Assets/Scripts/Menu/Transition.cs
+++ Assets/Scripts/Menu/Transition.cs
@@ -5,13 +5,17 @@ public class Transition : MonoBehaviour
 {
     public GameObject[] buttons;
     Animator anim;
-	
+
+    private string[] currentScene = {"Menu", "Config"};
     public void ChangeScene(string scene)
     {
 
-		foreach (GameObject g in buttons)
-		{
-			g.GetComponent<Animator>().SetBool("Go"+scene, !g.GetComponent<Animator>().GetBool("Go"+scene));
-		}
+        foreach (GameObject g in buttons)
+        {
+            foreach (string s in currentScene)
+            {
+                g.GetComponent<Animator>().SetBool("Go" + s, !g.GetComponent<Animator>().GetBool("Go" + s));
+            }
+        }
     }
 }
